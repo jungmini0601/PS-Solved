fad3f198827b98e7f28af2d4f7e353e8d447089a
jungmini0601 <126523988+jungmini0601@users.noreply.github.com>
2023-03-02 19:48:37 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n - i; j++) sb.append(" ");
+            for(int j = 1; j <= i; j++) sb.append("*");
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
